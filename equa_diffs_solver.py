08981d85815cea3ea5b05be1bfecc29be77ba64c
Matro123 <mat.ros@outlook.fr>
2021-06-18 11:54:01 +0400

    Co-authored-by: Schlogel <Schlogel@users.noreply.github.com>

--- equa_diffs_solver.py
+++ equa_diffs_solver.py
@@ -23,15 +23,65 @@ class Polynome:
 
     def __init__(self, expr):
         expr_list = expr.replace(" ","").replace("-","+-").split("+")
+        expr_list = [valeur for valeur in expr_list if valeur != ""]
         expr_list.sort(key=deg, reverse=True)
         self.deg = deg(expr_list[0])
+        self.expr_list = expr_list
         self.expr = " + ".join(expr_list).replace("+ -","- ")
 
     def __add__(self, polynome):
-        return "bonjour"
+        expression_finale=[]
+        self_expr_copie = self.expr_list[:]
+        polynome_expr_copie = polynome.expr_list[:]
+        for mono1 in self.expr_list:
+            for mono2 in polynome.expr_list:
+                if deg(mono1) == deg(mono2):
+                    if deg(mono1) == 0:
+                        nouveau_monome = f'{int(mono1.split("X")[0]) + int(mono2.split("X")[0])}'
+                        self_expr_copie.remove(mono1)
+                        polynome_expr_copie.remove(mono2)
+                    elif deg(mono1) == 1:
+                        nouveau_monome = f'{int(mono1.split("X")[0]) + int(mono2.split("X")[0])}X'
+                        self_expr_copie.remove(mono1)
+                        polynome_expr_copie.remove(mono2)
+                    else:
+                        nouveau_monome = f'{int(mono1.split("X")[0]) + int(mono2.split("X")[0])}X^{deg(mono1)}'
+                        self_expr_copie.remove(mono1)
+                        polynome_expr_copie.remove(mono2)
+                    expression_finale.append(nouveau_monome)
+        for mono1 in self_expr_copie:
+            expression_finale.append(mono1)
+        for mono2_restant in polynome_expr_copie:
+            expression_finale.append(mono2_restant)
 
+        return Polynome("+".join(expression_finale))
+                    
     def __sub__(self, polynome):
-        return "au revoir"
+        expression_finale=[]
+        self_expr_copie = self.expr_list[:]
+        polynome_expr_copie = polynome.expr_list[:]
+        for mono1 in self.expr_list:
+            for mono2 in polynome.expr_list:
+                if deg(mono1) == deg(mono2):
+                    if deg(mono1) == 0:
+                        nouveau_monome = f'{int(mono1.split("X")[0]) - int(mono2.split("X")[0])}'
+                        self_expr_copie.remove(mono1)
+                        polynome_expr_copie.remove(mono2)
+                    elif deg(mono1) == 1:
+                        nouveau_monome = f'{int(mono1.split("X")[0]) - int(mono2.split("X")[0])}X'
+                        self_expr_copie.remove(mono1)
+                        polynome_expr_copie.remove(mono2)
+                    else:
+                        nouveau_monome = f'{int(mono1.split("X")[0]) - int(mono2.split("X")[0])}X^{deg(mono1)}'
+                        self_expr_copie.remove(mono1)
+                        polynome_expr_copie.remove(mono2)
+                    expression_finale.append(nouveau_monome)
+        for mono1 in self_expr_copie:
+            expression_finale.append(mono1)
+        for mono2 in polynome_expr_copie[:-1]:
+            expression_finale.append("-" + mono2)
+
+        return Polynome("+".join(expression_finale))
 
     def __repr__(self):
         """
