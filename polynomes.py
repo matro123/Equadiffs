696e13c7cc3e21d00e337de1d45e0eba6f8a1646
Matro123 <mat.ros@outlook.fr>
2021-06-27 16:01:48 +0400

    nouvelle soustraction, début de la DE mais ne marche pas encore. Multiplication par un scalaire

--- polynomes.py
+++ polynomes.py
@@ -53,7 +53,6 @@ def convert(nombre):
 
     return float(nombre)
 
-
 class Polynome:
     """
     Une classe pour représenter un polynôme
@@ -66,8 +65,12 @@ class Polynome:
     --------
         __add__ :
             addition des polynômes
+        __neg__ :
+            inverse le signe de chaque monome du polynome
         __sub__ :
             soustraction des polynômes
+        __mul__ :
+            multiplication des polynomes
         derivee :
             donne le polynome derivé
         primitive :
@@ -104,49 +107,59 @@ class Polynome:
             expression_finale.remove("0")
         return Polynome("+".join(expression_finale))
 
-    def __sub__(self, polynome):
+    def __neg__(self):
         expression_finale = []
-        self_expr_copie = self.expr_list[:]
-        polynome_expr_copie = polynome.expr_list[:]
-        for mono1 in self.expr_list:
-            for mono2 in polynome.expr_list:
-                if deg(mono1) == deg(mono2):
-                    if deg(mono1) == 0:
-                        nouveau_monome = f'{convert(mono1.split("X")[0]) - convert(mono2.split("X")[0])}'
-                        self_expr_copie.remove(mono1)
-                        polynome_expr_copie.remove(mono2)
-                    elif deg(mono1) == 1:
-                        nouveau_monome = f'{convert(mono1.split("X")[0]) - convert(mono2.split("X")[0])}X'
-                        self_expr_copie.remove(mono1)
-                        polynome_expr_copie.remove(mono2)
-                    else:
-                        nouveau_monome = f'{convert(mono1.split("X")[0]) - convert(mono2.split("X")[0])}X^{deg(mono1)}'
-                        self_expr_copie.remove(mono1)
-                        polynome_expr_copie.remove(mono2)
-                    expression_finale.append(nouveau_monome)
-        for mono1 in self_expr_copie:
-            expression_finale.append(mono1)
-        for mono2 in polynome_expr_copie[:-1]:
-            expression_finale.append("-" + mono2)
+        for monome in self.expr_list:
+            if not "-" in monome:
+                expression_finale.append("-" + monome)
+            else:
+                expression_finale.append(monome[1:])
         return Polynome("+".join(expression_finale))
 
-    def __mul__(self, polynome):
-        expression_presque_finale = []
-        for mono1 in self.expr_list:
-            for mono2 in polynome.expr_list:
-                if "-∞" in (deg(mono1), deg(mono2)):
-                    nouveau_monome = "0"
-                elif deg(mono1) + deg(mono2) == 0:
-                    nouveau_monome = f'{convert(mono1.split("X")[0]) * convert(mono2.split("X")[0])}'
-                elif deg(mono1) + deg(mono2) == 1:
-                    nouveau_monome = f'{convert(mono1.split("X")[0]) * convert(mono2.split("X")[0])}X'
-                else:
-                    nouveau_monome = f'{convert(mono1.split("X")[0]) * convert(mono2.split("X")[0])}X^{deg(mono1)+deg(mono2)}'
-                expression_presque_finale.append(Polynome(nouveau_monome))
-        polynome_final = Polynome("0")
-        for monome in expression_presque_finale:
-            polynome_final += monome
-        return polynome_final
+    def __sub__(self, polynome):
+        return self + polynome.__neg__()
+
+    def __mul__(self, autre):
+        if isinstance(autre, Polynome):
+            expression_presque_finale = []
+            for mono1 in self.expr_list:
+                for mono2 in autre.expr_list:
+                    if "-∞" in (deg(mono1), deg(mono2)):
+                        nouveau_monome = "0"
+                    elif deg(mono1) + deg(mono2) == 0:
+                        nouveau_monome = f'{convert(mono1.split("X")[0]) * convert(mono2.split("X")[0])}'
+                    elif deg(mono1) + deg(mono2) == 1:
+                        nouveau_monome = f'{convert(mono1.split("X")[0]) * convert(mono2.split("X")[0])}X'
+                    else:
+                        nouveau_monome = f'{convert(mono1.split("X")[0]) * convert(mono2.split("X")[0])}X^{deg(mono1)+deg(mono2)}'
+                    expression_presque_finale.append(Polynome(nouveau_monome))
+            polynome_final = Polynome("0")
+            for monome in expression_presque_finale:
+                polynome_final += monome
+            return polynome_final
+
+        if isinstance(autre, int):
+            expression_finale = []
+            for monome in self.expr_list:
+                coef = monome.split("X")[0]
+                if coef == "":
+                    coef = 1
+                expression_finale.append(f'{convert(coef) * autre}X^{deg(monome)}')
+            return Polynome("+".join(expression_finale))
+    
+    def __rmul__(self, autre):
+            return self * autre
+
+    def __floordiv__(self, autre): # ne marche pas encore
+        if self.deg < autre.deg:
+            return Polynome("0")
+        reste = self
+        quotient = Polynome("0")
+        if self.deg == autre.deg:
+            return Polynome("1")
+        while reste.deg >= autre.deg:
+            reste = reste - autre
+        return reste
 
     def derivee(self):
         """
@@ -202,6 +215,7 @@ class Polynome:
                 tartiflette = nombre.split("X") #  On a plus d'inspi mdr
                 nombre = tartiflette[0].replace("Fract", "").replace("(", "").replace(")", "").split(",")
                 expr_list[i] = f'{Fract(*[int(n) for n in nombre])}X{tartiflette[1]}'
+            expr_list[i] = nombre.replace("X^0", "").replace("X^1", "X")
         if "0" not in expr_list:
             expr_list.sort(key=deg, reverse=True)
         self.deg = deg(expr_list[0])
